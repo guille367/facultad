6429d85c301ee8a47d58d6f0546979ee7afd925d
guille367 <ponceandresguillermo@hotmail.com>
2016-04-07 00:09:37 -0300

    falta el ultimo ejercicio

--- ejercicios/app.js
+++ ejercicios/app.js
@@ -29,30 +29,61 @@ var MAIN = (function (DATA) {
 
   // Retornar un array de objetos que solo contengan las claves name, email y age, de todos los usuarios mayores que 'age'
   lib.userOlderThan = function (age) {
-      return DATA.map(function(user){
-            if(user.age > age)
-                return { null }
-      })
+      return DATA
+            .filter(function(user){
+            return user.age > age;
+        })
+      .map(function(user){
+                return { nombre: user.name , mail: user.email , edad: user.age };
+      });
+
   };
 
   // Retornar un objeto que contenga solo el nombre y la edad (name y age) del usuario mas grande.
   lib.olderUser = function (age) {
-
+      return DATA.reduce(function(ant,act,indice,arr){
+          if(act.age >= ant.age)
+              return { name: act.name , age: act.age }; 
+          
+          return { name: ant.name , age: ant.age };
+      },DATA[0]);
   };
 
   // Retornar el promedio de edad de los usuarios (number)
   lib.userAgeAverage = function (age) {
-
+      var arr = DATA
+            .reduce(function(act,ant,indice){
+             return { age: (ant.age += act.age) , total: indice + 1};
+      },DATA[0]);
+       
+      return arr.age / arr.total;
   };
 
   // Retornar el promedio de edad de los usuarios hombres (number)
   lib.userMaleAgeAverage = function (age) {
-
+      
+      var arr = DATA.filter(function(user){
+          return user.gender === 'male';
+      })
+      
+      arr = arr.reduce(function(ant,act,indice){
+          return { age: (ant.age += act.age), total : (indice + 1)  }
+      },arr[0]);
+      
+      return arr.age / arr.total;
   };
 
   // Retornar el promedio de edad de los usuarios mujeres (number)
   lib.userFemaleAgeAverage = function (age) {
+    var arr = DATA.filter(function(user){
+              return user.gender === 'female';
+          })
 
+          arr = arr.reduce(function(ant,act,indice){
+              return { age: (ant.age += act.age), total : (indice + 1)  }
+          },arr[0]);
+
+      return arr.age / arr.total;
   };
 
   // Retornar un objeto  de etiquetas (tags)
